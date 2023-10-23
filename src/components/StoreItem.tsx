f6504c600119032f9169cee2b842ff7378a6be0b
Ibraheem Qadi <ibra444hem@gmail.com>
2023-10-23 07:14:52 +0300

    Create a currency format utility

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,4 +1,5 @@
 import { Card } from "react-bootstrap";
+import { formatCurrency } from "../utilities/formatCurrency";
 
 export type StoreItemProps = {
   id: number;
@@ -19,9 +20,9 @@ const StoreItem = ({ name, imgUrl, price }: StoreItemProps) => {
       <Card.Body>
         <Card.Title style={styles.cardTitle}>
           <Card.Text className="fs-2">{name}</Card.Text>
-          <Card.Text className="ms-2 text-muted">${price}</Card.Text>
+          <Card.Text className="ms-2 text-muted">{formatCurrency(price)}</Card.Text>
         </Card.Title>
-        <button className="btn btn-primary">Add to cart</button>
+        <button className="btn btn-primary w-100">Add to cart</button>
       </Card.Body>
     </Card>
   );
