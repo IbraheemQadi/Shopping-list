bd294438b601fa192a55a7c27cc6ef642fbec8b0
Ibraheem Qadi <ibra444hem@gmail.com>
2023-10-26 11:12:09 +0300

    Add delete button for cart item

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,6 +1,7 @@
 import { Stack } from "react-bootstrap";
 import { useShoppingCart } from "../context/ShoppingCartContext";
 import { formatCurrency } from "../utilities/formatCurrency";
+import { RxCross2 } from "react-icons/rx";
 
 type CartItemProps = {
   id: number;
@@ -14,50 +15,66 @@ const styles: { [key: string]: React.CSSProperties } = {
     marginRight: "5px",
     objectFit: "cover",
   },
+  delete: {
+    width: "20px",
+    height: "20px",
+    color: "white",
+    borderRadius: "50%",
+    backgroundColor: "red",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    cursor: "pointer",
+    position: "absolute",
+    right: "0",
+    top: "-5px",
+  },
 };
 
 const CartItem = ({ id, quantity }: CartItemProps) => {
-  const { increaseItemQuantity, decreaseItemQuantity, storeItems } =
-    useShoppingCart();
+  const {
+    increaseItemQuantity,
+    decreaseItemQuantity,
+    removeFromCart,
+    storeItems,
+  } = useShoppingCart();
+
   const item = storeItems.find((item) => item.id === id);
   if (!item) return null;
 
   return (
     <div className="d-flex align-items-center justify-content-between">
-      <div>
-        <Stack direction="horizontal">
-          <img src={item.imgUrl} style={styles.img} />
-          <div>
-            <span>{item.name}</span>
-            <p className="text-muted mb-0">
-              {formatCurrency(item.price * quantity)}
-            </p>
+      <Stack direction="horizontal">
+        <div className="position-relative">
+          <div onClick={() => removeFromCart(id)} style={styles.delete}>
+            <RxCross2 size={"1rem"} />
           </div>
-        </Stack>
-      </div>
-      <div>
-        <div className="d-flex align-items-center">
-          <button
-            onClick={() => decreaseItemQuantity(id)}
-            className="btn btn-sm btn-outline-secondary me-2"
-          >
-            -
-          </button>
-          <p className="mb-0">{quantity}</p>
-          <button
-            onClick={() => increaseItemQuantity(id)}
-            className="btn btn-sm btn-outline-secondary ms-2"
-          >
-            +
-          </button>
+          <img src={item.imgUrl} style={styles.img} />
         </div>
+        <div>
+          <span>{item.name}</span>
+          <p className="text-muted mb-0">
+            {formatCurrency(item.price * quantity)}
+          </p>
+        </div>
+      </Stack>
+      <div className="d-flex align-items-center">
+        <button
+          onClick={() => decreaseItemQuantity(id)}
+          className="btn btn-sm btn-outline-secondary me-2"
+        >
+          -
+        </button>
+        <p className="mb-0">{quantity}</p>
+        <button
+          onClick={() => increaseItemQuantity(id)}
+          className="btn btn-sm btn-outline-secondary ms-2"
+        >
+          +
+        </button>
       </div>
     </div>
   );
 };
 
 export default CartItem;
-
-// <div className="mb-1">
-//   <button className="btn btn-sm btn-outline-danger w-100">Remove</button>
-// </div>;
